793ad5ed58b53443daeadc7517eb9f1dae625bfa
Omar Amr <omaradel9545@gmail.com>
2017-12-31 04:22:02 +0100

    [ADD] rbf and linear kernels added

--- EX6.R
+++ EX6.R
@@ -1,4 +1,5 @@
 library(kernlab)
+library(kebabs)
 
 directory <- '~/Desktop/ML-Assign-4-EX-6'
 setwd(directory)
@@ -26,9 +27,8 @@ count_occurances <- function(subsquence, subsequences_list)
 }
 
 # ksvm
-train_model <- function()
+train_spectrum_model <- function()
 {
-  
   A <- matrix(nrow = matrix_size, ncol = matrix_size, byrow = TRUE)
   
   for (x in 1:matrix_size)
@@ -46,13 +46,12 @@ train_model <- function()
       A[x, y] <- match_score
     }
   }
-  
   A <- as(A, "KernelMatrix")
-  trained_model <<- ksvm(A,training_labels,type="C-svc",C=100,scaled=c(), cross=10)
+  spectrum_trained_model <<- ksvm(A,training_labels,type="C-svc",C=100,scaled=c(), cross=10)
 }
 
 
-predict_acid <- function(input_acid)
+spectrum_model_prediction <- function(input_acid)
 {
   x_subsequences <- get_all_subsequences(subsquence_length, input_acid)
   M <- matrix(nrow = 1, ncol = matrix_size, byrow = TRUE)
@@ -67,92 +66,104 @@ predict_acid <- function(input_acid)
     M[1, y] <- match_score
   }
   
-  acid_prediction = predict(trained_model,M)
+  acid_prediction <- predict(spectrum_trained_model,M)
   return(acid_prediction)
 }
 
-# TODO: Rempve comment bellow to train model
-# train_model()
+
+start <- Sys.time()
+train_spectrum_model()
+end <- Sys.time()
+print(end - start)
+
 
 # -------------------------------------------------- #
-
 # One hot encoding
-acid_alphabet <- vector()
-acid_length <- nchar(training_data[1])
 
-for(acid in training_data)
+
+get_alphabet <- function()
 {
-  temp <- unique(unlist(strsplit(acid, "")))
-  acid_alphabet <- unique(append(acid_alphabet, temp))
-}
-acid_alphabet <- sort(acid_alphabet)
-
-encoding_matrix <- matrix(nrow=matrix_size, ncol=acid_length*length(acid_alphabet),byrow =TRUE)
-
-for (data_sample_counter in 1:matrix_size) 
-{
-  data_sample_split <- unlist(strsplit(training_data[data_sample_counter], ""))
-  data_sample_vect <- vector()
-  data_sample_encoding = replicate(length(acid_alphabet), "0")
-  for (char in data_sample_split) 
+  for(acid in training_data)
   {
-    temp_encoding = replicate(length(acid_alphabet), "0")
+    temp <- unique(unlist(strsplit(acid, "")))
+    acid_alphabet <<- unique(append(acid_alphabet, temp))
+  }
+  acid_alphabet <<- sort(acid_alphabet)
+}
+
+
+encode_sequence <- function(input_sequence)
+{
+  data_sample_split <- unlist(strsplit(input_sequence, ""))
+  data_sample_vect <- vector()
+  data_sample_encoding <- replicate(length(acid_alphabet), "0")
+  for (char in data_sample_split)
+  {
+    temp_encoding <- replicate(length(acid_alphabet), "0")
     temp_encoding[match(char, acid_alphabet)] <- 1
     temp_encoding <- unlist(strsplit(temp_encoding, ""))
     data_sample_vect <- append(data_sample_vect, temp_encoding)
   }
-  encoding_matrix[data_sample_counter, ] <- data_sample_vect
+  return(as.integer(data_sample_vect))
 }
-# TODO: Convert to numeric in the loop
-# encoding_matrix <- mapply(encoding_matrix, FUN=as.numeric)
-# trained_model <<- ksvm(encoding_matrix,training_labels,type="C-svc",C=100,scaled=c(), kernel='rbfdot')
-
-# -------------------------------------------------- #
-# Kebabs
-
-# library(kebabs)
-
-# temp_train <- training_set[c(1:1800),  ]
-# temp_test <- training_set[c(1801:2000),  ]
-# training_labels <- temp_train[, 2]
-# A <- matrix(nrow = 1800, ncol = 1800, byrow = TRUE)
 
 
-# for (x in 1:1800)
-# {
-#   # temp_dataset <- training_set[-c(x),]
-#   for (y in 1:1800)
-#   {
-#    
-#       x_subsequences = get_all_subsequences(subsquence_length, temp_train[x, 1])
-#       y_subsequences = get_all_subsequences(subsquence_length, temp_train[y, 1])
-#       match_score <- 0
-#       for (sub in x_subsequences)
-#       {
-#         match_score <- match_score + (count_occurances(sub, x_subsequences) * count_occurances(sub, y_subsequences))
-#       }
-#       A[x, y] <- match_score
-#   }
-# }
+calculate_encoded_matrix <- function()
+{
+  for (data_sample_counter in 1:matrix_size)
+  {
+    encoding_matrix[data_sample_counter, ] <<- encode_sequence(training_data[data_sample_counter])
+  }
+  print("Data encoding done")
+}
 
-# model <- kebabs:::svmd.default(A, training_labels, cost=1)
 
-# B = matrix(nrow = 200, ncol = 1800, byrow = TRUE)
-# for (x in 1:200)
-# {
-#   # temp_dataset <- training_set[-c(x),]
-#   for (y in 1:1800)
-#   {
-#     x_subsequences = get_all_subsequences(subsquence_length, temp_test[x, 1])
-#     y_subsequences = get_all_subsequences(subsquence_length, temp_train[y, 1])
-#     match_score <- 0
-#     for (sub in x_subsequences)
-#     {
-#       match_score <- match_score + (count_occurances(sub, x_subsequences) * count_occurances(sub, y_subsequences))
-#     }
-#     B[x, y] <- match_score
-#   }
-# }
-# 
-# B <- as(B, "KernelMatrix")
+train_linear_model <- function()
+{
+  linear_trained_model <<- ksvm(encoding_matrix,training_labels,type="C-svc",C=100, kernel='vanilladot', cross=10)
+  print("Linear Model Training Done")
+}
 
+
+train_rbf_model <- function()
+{
+  rbf_trained_model <<- ksvm(encoding_matrix,training_labels,type="C-svc",C=50,kpar=list(sigma=0.001), kernel='rbfdot', cross=10)
+  print("RBF Model Training Done")
+}
+
+
+linear_model_prediction <- function(input_acid)
+{
+  encoded <- encode_sequence(input_acid)
+  prediction_mat <- matrix(nrow=1, ncol=acid_length*length(acid_alphabet),byrow =TRUE)
+  prediction_mat[1, ] <- encoded
+  return(predict(linear_trained_model,prediction_mat))
+}
+
+rbf_model_prediction <- function(input_acid)
+{
+  encoded <- encode_sequence(input_acid)
+  prediction_mat <- matrix(nrow=1, ncol=acid_length*length(acid_alphabet),byrow =TRUE)
+  prediction_mat[1, ] <- encoded
+  return(predict(rbf_trained_model,prediction_mat))
+}
+
+
+acid_alphabet <- vector()
+acid_length <- nchar(training_data[1])
+get_alphabet()
+encoding_matrix <- matrix(nrow=matrix_size, ncol=acid_length*length(acid_alphabet),byrow =TRUE)
+calculate_encoded_matrix()
+
+# start <- Sys.time()
+# ain_linear_model()
+# train_rbf_model()
+# end <- Sys.time()
+# print(end - start)
+
+# Prediction example
+
+to_predict <- "FGEKIGLSFQLADDL"
+# linear_model_prediction(to_predict)
+# rbf_model_prediction(to_predict)
+spectrum_model_prediction(to_predict)
